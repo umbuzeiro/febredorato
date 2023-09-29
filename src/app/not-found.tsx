9641784774e1e176710db555d1655e3cace917a0
George <george@cuck.com.br>
2023-09-29 15:18:41 -0300

    404 page fix.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,6 @@ import { Section } from "@/components/Section";
 export default function NotFound(){
 
     return <>
-    <Header/>
     <Section title="404">
     <h2 className="ml-1 text-md font-bold">Conteúdo não encontrado.</h2>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,21 @@ export default function Home() {
     <Section title="Febre do Rato">
       <h1>Website em construção</h1>
 
-    <div className=' mt-10 p-5 ml-6 bg-white w-1/3 rounded-lg drop-shadow-sm'>
-      <h2 className='font-semibold text-md'>Conheça o conglomerado:</h2>
-    <ul className="list-disc ml-4">
-  <li>Febre do Rato Serviços Elétricos</li>
-  <li>Febre do Rato Energias Renováveis</li>
-  <li>Febre do Rato Refrigeração</li>
-  <li>Febre do Rato Transmissão</li>
-  <li>Febre do Rato Termelétrica</li>
-  <li>Febre do Rato Cursos</li>
-  <li>Febre do Rato Engenharia</li>
-  <li>Febre do Rato Participações</li>
-  
-</ul>
-    </div>
-      
-      </Section>
+      <div className=' mt-10 p-5 ml-6 bg-white w-1/3 rounded-lg drop-shadow-sm'>
+        <h2 className='font-semibold text-md'>Conheça o conglomerado:</h2>
+        <ul className="list-disc ml-4">
+          <li>Febre do Rato Serviços Elétricos</li>
+          <li>Febre do Rato Energias Renováveis</li>
+          <li>Febre do Rato Refrigeração</li>
+          <li>Febre do Rato Transmissão</li>
+          <li>Febre do Rato Termelétrica</li>
+          <li>Febre do Rato Cursos</li>
+          <li>Febre do Rato Engenharia</li>
+          <li>Febre do Rato Participações</li>
+
+        </ul>
+      </div>
+
+    </Section>
   )
 }
