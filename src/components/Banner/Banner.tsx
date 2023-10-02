52858578279d470520aa59db0029e6a839b137b4
George <george@cuck.com.br>
2023-10-02 13:32:45 -0300

    Optimized images.

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 
 interface BannerProps {
@@ -9,10 +10,14 @@ interface BannerProps {
 
 export function Banner({ title, desc, url, img }: BannerProps) {
     return (
+        //style={{ backgroundImage: `url(${img})` }}
         <>
             <section style={{ backgroundImage: `url(${img})` }} className="bg-no-repeat bg-center bg-cover mx-5 m-10 rounded-[18px] ">
                 <div className="z-0 rounded-[18px] bg-gradient-to-r from-emerald-950/50 to-emerald-700/10 drop-shadow-lg lg:h-[26rem] md:h-[30rem] sm:h-[28rem] min-h-full text-left flex flex-col items-start place-content-left place-content-between text-white">
-
+                    {/* <Image className="relative -z-10 bg-no-repeat bg-center bg-cover rounded-[18px]" src={`${img}`} 
+                    alt="se"
+                    fill
+                    style={{objectFit:"cover"}}/> */}
                     <h1 className="md:mx-20 sm:mx-4 mt-20 sm:mt-10 sm:text-2xl md:text-3xl font-bold my-3">{title}</h1>
                     <h2 className="md:mx-20 sm:mx-4 sm:mt-10 sm:text-sm md:text-lg">{desc}</h2>
                     <Link className="md:mx-20 sm:mx-4 mb-10 sm:mt-10 sm:text-sm md:text-1xl font-bold my-3 px-5 bg-gradient-to-r from-emerald-500 to-emerald-600 p-3 rounded-full" href={`${url}`}>Saiba mais</Link>
--- src/components/List/Sector.tsx
+++ src/components/List/Sector.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 
 interface SectorProps{
@@ -8,8 +9,10 @@ interface SectorProps{
 export function Sector({title, img} : SectorProps) {
 
     return (
+        // style={{backgroundImage: `url(${img})`}}
         <>
-            <section style={{backgroundImage: `url(${img})`}} className="rounded-lg bg-no-repeat bg-center bg-cover hover:scale-105 transition ease-in-out duration-300">
+            <section style={{backgroundImage: `url(${img})`}} className="rounded-lg bg-no-repeat bg-center bg-cover hover:scale-105 transition ease-in-out duration-300 z-0">
+            
                 <Link href="/">
                 <div className="bg-gradient-to-r from-emerald-950/30 to-emerald-700/10 drop-shadow-lg rounded-lg  sm:h-64 md:h-60 lg:h-80 min-h-full text-center flex items-end place-content-center ">
                     <p className=" drop-shadow-sm font-bold text-white sm:text-3xl md:text-lg lg:text-3xl p-4 mb-4">{title}</p>
