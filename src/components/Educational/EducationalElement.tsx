71bd624c434214b73c0581d032a2bfc0b487f50c
George <george@cuck.com.br>
2023-10-07 12:51:52 -0300

    Banner height fix.

--- src/components/Educational/EducationalElement.tsx
+++ src/components/Educational/EducationalElement.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { ArrowUpRight } from "lucide-react"
 
 interface EducationalProps {
     title?: string,
@@ -14,6 +15,8 @@ export function EducationalElement({ title, img }: EducationalProps) {
                 <Link href="/">
                     <div className="bg-gradient-to-r from-emerald-950/30 to-emerald-700/10 drop-shadow-lg rounded-lg sm:h-64 md:h-60 lg:h-80 text-center flex items-end place-content-center ">
                         <p className=" drop-shadow-sm font-bold text-white sm:text-3xl md:text-lg lg:text-3xl p-4 mb-4">{title}</p>
+                        
+        {/* <p className="text-white absolute text-left w-full p-6"><ArrowUpRight size={32}/></p> */}
                     </div>
                 </Link>
 
